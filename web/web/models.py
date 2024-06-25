dc1736759fcff02482899f2e238719dbb6acfc2f
zolcano <87764175+zolcano@users.noreply.github.com>
2024-06-25 09:21:39 +0200

    Update models.py

--- web/web/models.py
+++ web/web/models.py
@@ -3,7 +3,7 @@ from django.db import models
 class Capteurs(models.Model):
     Id = models.AutoField(primary_key=True)
     Nom = models.CharField(max_length=250, blank=True, null=True)
-    Piece = models.CharField(max_length=50, blank=True, null=True)
+    Piece = models.CharField(max_length=50, unique=True, blank=True, null=True)
     Emplacement = models.CharField(max_length=50, blank=True, null=True)
     Date = models.CharField(max_length=50, blank=True, null=True)
         
@@ -15,7 +15,7 @@ class Capteurs(models.Model):
     
 class Donnees(models.Model):
     Id = models.AutoField(primary_key=True)
-    CapteurID = models.CharField(max_length=50, blank=True, null=True)
+    CapteurID = models.CharField(max_length=50, unique=True, blank=True, null=True)
     Timestamp = models.CharField(max_length=50, blank=True, null=True)
     Valeur = models.CharField(max_length=50, blank=True, null=True)
         
